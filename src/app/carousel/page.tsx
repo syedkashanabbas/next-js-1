5c98af17db9150cbd99f438e6cac26c390cedee1
SyedKashanAbbasNaqvi <abbaskashan234@gmail.com>
2025-08-11 01:32:04 +0500

    feat: add carousel component with styles and UI updates

---
+++ src/app/carousel/page.tsx
@@ -0,0 +1,24 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export default function Page() {
+    return (
+        <div>
+
+            <Carousel>
+  <CarouselContent>
+    <CarouselItem>1</CarouselItem>
+    <CarouselItem>2</CarouselItem>
+    <CarouselItem>3</CarouselItem>
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
+        </div>
+    );
+}
